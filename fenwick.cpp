830576cd95fb58eaa23d444106baf7f6205bd544
Raj Singh <rajsinghast03@gmail.com>
2024-03-19 20:29:34 +0530

    Added Fenwick Tree

---
+++ fenwick.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int BIT[1000], a[1000], n;
+void update(int x, int delta)
+{
+      for(; x <= n; x += x&-x)
+        BIT[x] += delta;
+}
+int query(int x)
+{
+     int sum = 0;
+     for(; x > 0; x -= x&-x)
+        sum += BIT[x];
+     return sum;
+}
+
+int main()
+{
+     cin >> n;
+
+    for(int i = 1;i<=n;++i) {
+      cin >> a[i];
+      update(i,a[i]);
+    }
+
+    cout << query(4);
+}
